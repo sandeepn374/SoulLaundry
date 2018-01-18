419ea3df14ad7d1356520e4a03b95a22f22b8066
kshravi86 <18forever>
2018-01-18 15:30:31 +0530

    changes to added textview

--- app/src/main/java/com/soullaundry/CollectionActivity.java
+++ app/src/main/java/com/soullaundry/CollectionActivity.java
@@ -132,13 +132,29 @@ public class CollectionActivity extends Activity
 		qty = (EditText) findViewById(com.soullaundry.R.id.qty);
 		deltype=(Spinner)findViewById(R.id.deltype);
 		deldays=(Spinner)findViewById(R.id.deldays);
+        cloth.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                //Change the selected item's text color
+                ((TextView) view).setTextSize(18);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
 
 
 
 
 
 
-		price.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        price.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
 
