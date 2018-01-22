b9886ec7df5df6c12411929753ffce87c60e9084
kshravi86 <18forever>
2018-01-22 14:20:15 +0530

    some changes

--- app/src/main/java/com/soullaundry/DeliveryActivity.java
+++ app/src/main/java/com/soullaundry/DeliveryActivity.java
@@ -236,7 +236,7 @@ public class DeliveryActivity extends Activity implements SearchView.OnQueryText
 
 
                             paid.setTextColor(Color.BLACK);
-                            paid.setGravity(Gravity.LEFT);
+                            paid.setGravity(Gravity.RIGHT);
                             paid.setLayoutParams(params);
                             paid.setWidth(30);
                             paid.setHeight(30);
