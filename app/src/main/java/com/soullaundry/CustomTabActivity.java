d9e75c402fc156864e014d3e6f2c8926ffe9f1c7
kshravi86 <18forever>
2018-01-24 15:47:27 +0530

    some changes

--- app/src/main/java/com/soullaundry/CustomTabActivity.java
+++ app/src/main/java/com/soullaundry/CustomTabActivity.java
@@ -77,6 +77,8 @@ public class CustomTabActivity extends AppCompatActivity {
 
 
 
+
+
     }
 
 
--- app/src/main/java/com/soullaundry/ViewPagerAdapter.java
+++ app/src/main/java/com/soullaundry/ViewPagerAdapter.java
@@ -3,8 +3,10 @@ package com.soullaundry;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,6 +15,8 @@ import java.util.List;
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     private final List<Fragment> mFragmentList = new ArrayList<>();
     private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    HashMap<Integer, Fragment> mPageReferenceMap = new HashMap<>();
 
     public ViewPagerAdapter(FragmentManager manager) {
         super(manager);
@@ -43,6 +47,24 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
         return super.getItemPosition(object);
 
     }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+        mPageReferenceMap.put(position, fragment);
+        return fragment;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
+        mPageReferenceMap.remove(position);
+    }
+    public Fragment getFragment(int key) {
+        return mPageReferenceMap.get(key);
+    }
+
+
 
 
 }
