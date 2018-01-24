890fd05798541e7cc2e238722dca576bee3e1429
kshravi86 <18forever>
2018-01-24 12:26:24 +0530

    some changes

---
+++ app/src/main/java/com/soullaundry/StateVO.java
@@ -0,0 +1,26 @@
+package com.soullaundry;
+
+/**
+ * Created by kshravi on 24/01/2018 AD.
+ */
+
+public class StateVO {
+    private String title;
+    private boolean selected;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+}
